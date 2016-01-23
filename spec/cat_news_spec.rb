73230305db78cac40c2113e8b2f5ca6015c725e3
mmmeyers <marsi286@gmail.com>
2016-01-22 19:05:58 -0500

    Wrote some tests

--- spec/cat_news_spec.rb
+++ spec/cat_news_spec.rb
@@ -6,6 +6,30 @@ describe CatNews do
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
+  end
+end
+
+describe CatNews::Story do
+  before :each do
+    @story = CatNews::Story.new
+  end
+
+  describe "#new" do
+    it 'takes two parameters and returns a Story object' do
+      @story.should be_an_instance_of CatNews::Story
+    end
+  end
+
+  describe ".scrape_stories" do
+    it "returns an array of stories" do
+      expect(CatNews::Story.scrape_stories).to be_an_instance_of(Array)
+    end
+  end
+
+  describe ".scrape_first_story" do
+    it "returns an instance of Story" do
+      expect(CatNews::Story.scrape_first_story).to be_an_instance_of(CatNews::Story)
+    end
   end
 end
